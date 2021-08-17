90299f526a1290dfb51e40717a3de42c40d9b78d
Donovan Gorrocino <A00344371@itesm.mx>
2021-08-17 04:24:05 +0000

    Solve test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+
 func main() {
-	fmt.Println("Welcome to the jungle")
+	
+	//name := os.Args[1]
+	n := len(os.Args)
+	
+	if(n>1){
+		if n == 1{ 
+			name := os.Args[1]
+			fmt.Printf("Hello %v, Welcome to the jungle \n", name)
+		}else{
+			name := os.Args[1]
+			for i := 2; i < n; i++{
+				name += " " + os.Args[i]
+			}
+			fmt.Printf("Hello %v, Welcome to the jungle \n", name)	
+		}
+	}else{
+		fmt.Println("Error, No name was entered")
+	}	
 }
