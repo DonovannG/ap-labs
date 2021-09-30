704de96637424397257b5f2aad3b1b3f9207b30d
Donovan Gorrocino <A00344371@itesm.mx>
2021-09-30 05:54:49 +0000

    Solved c-str-lib

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int args, char* argv[]) {
+    char add[] = "-add", find[] = "-find";
+    if(args==4){
+        if(strcmp(argv[1], add)==0){
+            printf("Initial Lenght      : %d\n",mystrlen(argv[2]));
+            printf("New String          : %s\n",mystradd(argv[2],argv[3]));
+            printf("New Lenght          : %d\n",mystrlen(mystradd(argv[2],argv[3])));
+        }else if(strcmp(argv[1], find)==0){
+            if(mystrfind(argv[2], argv[3])!=1){
+                printf("[%s] string was found at [%d] position\n",argv[3],mystrfind(argv[2], argv[3]));
+            }else{
+                printf("[%s] string wasn´t found in [%s] string\n",argv[3],argv[2]);
+            }
+        }else{
+            printf("The only available options are [-add] and [-find], please use one of them\n");
+            return 0;
+        }
+        return 1;
+    }
+    printf("Two strings are needed for this to work...\n");
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 
 int mystrlen(char *str){
-    return 0;
+    int len = 0;
+    while(*str != '\0'){
+        len++;
+        str++;
+    }
+    return len;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int originLen = mystrlen(origin), addLen = mystrlen(addition), newLen = originLen + addLen;
+    char* newString = (char *)malloc(newLen + 1);
+    for(int i = 0; i < originLen; i++){
+        newString[i] = origin[i];
+    }
+    for(int j = originLen; j < newLen; j++){
+        newString[j] = addition[j-originLen];
+    }
+    return newString;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int originLen = mystrlen(origin), subLen = mystrlen(substr);
+    for(int i = 0; i <= originLen-subLen; i++){
+        int j = 0;
+        for(j; j < subLen; j++){
+            if(origin[i+j] != substr[j]){
+                break;
+            }
+        }
+        if(j == subLen){
+            return i;
+        }
+    }
+    return -1;
 }
